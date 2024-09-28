2deb6a82e69ca50c5f3ad4500b60b3b526665f0f
Wenemighty <157887244+Wenemighty@users.noreply.github.com>
2024-09-28 19:02:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('tapButton').addEventListener('click', function() {
+    alert('Mining started!');
+});
+
+document.getElementById('boostBtn').addEventListener('click', function() {
+    alert('Boost activated!');
+});
